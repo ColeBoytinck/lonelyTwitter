954c16166c94a013e9e755279747c003aad50127
ColeBoytinck <cboytinc@ualberta.ca>
2019-02-26 18:34:25 -0700

    Added UI testing

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -1,21 +1,30 @@
 package ca.ualberta.cs.lonelytwitter;
 
-import android.app.Activity;
-import android.test.ActivityInstrumentationTestCase2;
+import android.content.Intent;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import junit.framework.TestCase;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.rule.ActivityTestRule;
 
 /**
  * Created by wz on 14/09/15.
  */
-public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 {
+public class LonelyTwitterActivityTest {
 
-    public LonelyTwitterActivityTest() {
-        super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
+    @Rule
+    ActivityTestRule<LonelyTwitterActivity> activityRule = new ActivityTestRule<LonelyTwitterActivity>(LonelyTwitterActivity.class);
+
+    @Test
+    public void addtext() {
+
+        onView(withId(R.id.body)).perform(typeText("This is a test."));
     }
 
-    public void testStart() throws Exception {
-        Activity activity = getActivity();
-
-    }
 }
